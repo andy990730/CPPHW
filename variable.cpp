ec82ef2fc7199f87eb5b66a23f045a3cb185e31f
andy990730 <s1140546@dmhs.kh.edu.tw>
2026-04-13 10:01:12 +0800

    Create  variable.cpp

---
+++ variable.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int age = 16;
+	string name = "Bruce";
+	cout <<"年齡：" << age << "姓名:" << name << endl; 
+	return 0;
+}
