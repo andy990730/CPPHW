5e64bf86cd5caad16d34b07489fdbb830795afcf
andy990730 <s1140546@dmhs.kh.edu.tw>
2026-04-27 09:46:17 +0800

    Create your_name.cpp

---
+++ your_name.cpp
@@ -0,0 +1,14 @@
+nclu#ide <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    int a, b;
+    cout << "輸入姓名\n";
+    cin >> name;
+    cout << "hello," << name <<endl;
+     cout << "輸入a空格b姓名\n";
+     cin >> a >> b;
+     cout << "a是" << a << "b是" << b;
+  	return 0;
+}
